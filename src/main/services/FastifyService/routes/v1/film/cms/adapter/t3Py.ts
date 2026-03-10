57efece56e7b2762f9ff802eddeb175a1a419830
Hiram <wy-hai@qq.com>
2026-03-10 16:43:25 +0800

    fix: t3-py adapter use the port to replace the parameter keyword matching process

--- src/main/services/FastifyService/routes/v1/film/cms/adapter/t3Py.ts
+++ src/main/services/FastifyService/routes/v1/film/cms/adapter/t3Py.ts
@@ -112,8 +112,7 @@ class ConnectService extends PythonService {
     this.checkBinary();
     await this.installDep();
 
-    const args = ['main.py', '--ctrl-port', String(this.ctrlPort)];
-    const pids = await this.matchProcess(args.join(' '));
+    const pids = await this.matchPort(this.ctrlPort);
 
     if (pids.length) {
       this.pids = pids;
@@ -125,7 +124,7 @@ class ConnectService extends PythonService {
       await new Promise((resolve, reject) =>
         this.runSpawn(['main.py', '--ctrl-port', String(this.ctrlPort)], true, {
           stdoutCb: async () => {
-            const pids = await this.matchProcess(args.join(' '));
+            const pids = await this.matchPort(this.ctrlPort);
             if (pids.length) {
               this.pids = pids;
               await this.connect();
@@ -152,8 +151,7 @@ class ConnectService extends PythonService {
 
       // process
       if (!this.pids.length) {
-        const args = ['main.py', '--ctrl-port', String(this.ctrlPort)];
-        const pids = await this.matchProcess(args.join(' '));
+        const pids = await this.matchPort(this.ctrlPort);
         if (pids.length) this.pids = pids;
       }
 
